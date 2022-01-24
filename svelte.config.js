1820c8b7d9942a8c3a5c98ebed1a497d0ecccaa7
ziap <67962871+ziap@users.noreply.github.com>
2022-01-24 17:25:44 +0700

    Enable router

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,6 @@ const config = {
   kit: {
     adapter: adapter(),
     hydrate: false,
-    router: false,
     prerender: { enabled: false },
 
     // hydrate the <div id="svelte"> element in src/app.html
